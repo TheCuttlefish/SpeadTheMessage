6e6796dc752f7c4df59af0b21807e0480286d03d
Cuttlefish <zh4n@live.co.uk>
2018-01-28 15:08:33 +0000

    Main menu
    - menu
    - buttons
    - scripts

---
+++ Assets/scripts/MenuButtons.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+public class MenuButtons : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+	public void PlayGame(){
+		print ("play");
+		Application.LoadLevel(1);
+	}
+
+	public void QuitGame(){
+			print ("quit!!");
+			Application.Quit ();
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
